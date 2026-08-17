60007f1714c1d9eca318648dc802662edd36e492
Saikrishna <potharaboinasaikrishna1@gmail.com>
2026-08-17 13:58:32 +0530

    usermodel to User

--- src/models/user.js
+++ src/models/user.js
@@ -69,7 +69,7 @@ userSchema.methods.validatePassword = async function (passwordInputByUser) {
     const isPasswordValid = await bcrypt.compare(passwordInputByUser, passwordHash);
     return isPasswordValid;
 }
-const UserModel = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
 //1st place name of the model, 2nd what we created schema. 
 //when we use The model always first letter is Captal.
-module.exports = UserModel;
+module.exports = User;
