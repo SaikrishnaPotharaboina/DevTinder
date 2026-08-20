f733236af0a3bdc9df349620f3328f3685d2ef22
Saikrishna <potharaboinasaikrishna1@gmail.com>
2026-08-20 12:17:26 +0530

    Created  friendRequest API /request/send/:status/:toUserId routuer also some bug to fix

--- src/Routers/request.js
+++ src/Routers/request.js
@@ -1,14 +1,83 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const requestRouter = express.Router();
-const { userAuth } = require("../Middlewares/auth")
+const { userAuth } = require("../Middlewares/auth");
+const ConnectonRequest = require("../models/connectionRequest");
+const User = require("../models/user")
 
 
 
-requestRouter.post("/sendconnection", userAuth, async (req, res) => {
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+    try {
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
 
-    const user = req.user;
-    res.send(user.firstName + " : connction sent succesfull")
 
+
+
+        if (!mongoose.Types.ObjectId.isValid(toUserId)) {
+            return res.status(400).json({
+                message: "Invalid user ID"
+            });
+        };
+
+
+
+        if (fromUserId.toString() === toUserId.toString()) {
+            return res.status(400).json({
+                message: "You cannot send a connection request to yourself"
+            });
+        }
+
+
+        const allowedStatus = ["ignored", "interested"];
+
+        if (!allowedStatus.includes(status)) {
+            throw new Error("Status Invalid")
+        };
+
+
+
+        const exitingConnectionRequest = await ConnectonRequest.findOne({
+            $or: [
+
+                { fromUserId, toUserId },
+                { fromUserId: toUserId, toUserId: fromUserId },
+            ],
+        });
+
+        if (exitingConnectionRequest) {
+            return res.status(400).json({
+                message: "Connection request already exists"
+            })
+        };
+
+        const user = await User.findById(toUserId);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+
+
+        const connectonRequest = new ConnectonRequest({
+            fromUserId,
+            toUserId,
+            status,
+        });
+
+        const data = await connectonRequest.save();
+        return res.status(200).json({
+            message: "connecton sent successfull",
+            data
+        });
+
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 
