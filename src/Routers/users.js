277834d75eecdf47cb40aed31843cb5a53d39555
Saikrishna <potharaboinasaikrishna1@gmail.com>
2026-08-24 11:51:17 +0530

    created user connection api /user/connections

--- src/Routers/users.js
+++ src/Routers/users.js
@@ -21,7 +21,47 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
     } catch (error) {
         res.status(400).send(error.message);
     }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+
+    try {
+
+        const loggedinUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+
+            $or: [
+                {
+                    toUserId: loggedinUser._id,
+                    status: "accepted"
+                },
+                {
+                    fromUserId: loggedinUser._id,
+                    status: "accepted",
+                },
+            ],
+        })
+            .populate("fromUserId", ["firstName", "lastName"])
+            .populate("toUserId", ["firstName", "lastName"]);
+
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.toString() === loggedinUser._id.toString()) {
+
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+        res.status(200).json({
+            message: "Data fetched Successfull",
+            data
+        })
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 })
+
+
 
 
 module.exports = userRouter;
